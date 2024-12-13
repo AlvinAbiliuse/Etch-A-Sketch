b69389e833db220e4b9101b4f1b8f3e0e16cafac
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-14 01:56:28 +0400

    created html css and js files for etch a sketch and created a grid that tracks hover

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function createGrid(n) {
+	for (i=0; i < (n * n) ; i++) {
+		let grid = document.createElement("div");
+		grid.className = "gridElement";
+		container.appendChild(grid);
+	}
+}
+
+
+let color = "black";
+const container = document.querySelector("#container");
+container.addEventListener("mouseover", (e) => {
+	if (e.target.className == "gridElement") {
+		e.target.style.background = color;
+	}
+});
+
+createGrid(16);
