b1201331ffaa750df37af4b0ebbdfcee251fd91a
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-14 04:12:33 +0400

    progress on adding grid size change

--- script.js
+++ script.js
@@ -8,12 +8,42 @@ function createGrid(n) {
 }
 
 
+
 let color = "#292929";
-const container = document.querySelector("#container");
+let container = document.querySelector("#container");
 container.addEventListener("mouseover", (e) => {
 	if (e.target.className == "gridElement") {
 		e.target.style.background = color;
 	}
 });
 
+let newCanvas = document.querySelector("#newCanvasContainer");
+newCanvas.addEventListener("click", (e) => {
+		let gridSize = document.querySelector("input");
+		console.log(gridSize.value);
+		if (e.target.id == "plus") {
+			if (gridSize.value >= 30) {
+				gridSize.value = 30;
+			} else if (gridSize.value <= 0) {
+				gridSize.value = 1;
+			} else {
+				gridSize.value++;
+			}
+		} else if (e.target.id == "minus") {
+			if (gridSize.value <= 1) {
+				gridSize.value = 1;
+			} else if (gridSize.value > 30 ) {
+				gridSize.value = 1;
+			} else {
+				gridSize.value--;
+			}
+		} else if (e.target.id == "newCanvas") {
+			let gridItem = gridSize.value;
+			console.log(((480 / Number(gridSize.value)) / 2) + "px");
+			container.innerHTML = "";
+			createGrid(gridItem);
+			document.querySelector(".gridElement").style.padding = 
+				((480 / Number(gridSize.value)) / 2) + "px";
+		}
+});
 createGrid(16);
